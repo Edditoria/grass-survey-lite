b1123b048207cf8a49bbc2e7ebeef0db433da0a0
Edditoria <edditoria@gmail.com>
2018-02-18 00:03:03 +0800

    #style just keep good practice

--- docs/assets/js/calc.js
+++ docs/assets/js/calc.js
@@ -7,7 +7,7 @@ var calc = {}; // group all forumla into one object to avoid duplicated namespac
   * @param {Object} dms - expect { deg: 'ddd', min: 'mm', sec: 'ss' }
   * @returns {string} - a string consists of number from '0.0000' to '359.9999'
   */
-calc.dmsToDec = function (dms) {
+calc.dmsToDec = function(dms) {
 	// add '' as tail to convert number to string
 	return +dms.deg + (+dms.min / 60) + (+dms.sec / 3600) + '';
 };
