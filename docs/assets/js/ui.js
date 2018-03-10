b343cfb82bcea6f74fa1d6b94aa4999bf5f6ce65
Edditoria <edditoria@gmail.com>
2018-03-10 16:59:00 +0800

    add <form> to warp dmsToDec session in bearings page...
    - Also change the type of submit button to "button" from "submit"
    - Remove unnecessary code

--- docs/assets/js/ui.js
+++ docs/assets/js/ui.js
@@ -16,21 +16,6 @@ var dmsToDec = function() {
 };
 
 /**
-  * Reset value in text boxes in dms-dec convertion
-  */
-var resetDmsToDec = function() {
-	document.getElementById('dmsToDec-input-deg').value = '';
-	document.getElementById('dmsToDec-input-min').value = '';
-	document.getElementById('dmsToDec-input-sec').value = '';
-	document.getElementById('dmsToDec-output').value = 0;
-};
-
-/**
   * Trigger button-click to dmsToDec()
   */
 document.getElementById('dmsToDec-submit').onclick = dmsToDec;
-
-/**
-  * Trigger button-click to reset dms-dec form
-  */
-document.getElementById('dmsToDec-reset').onclick = resetDmsToDec;
