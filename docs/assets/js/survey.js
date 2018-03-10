e07cb8c21ebded9cf37be80ce3819bf2de55c407
Edditoria <edditoria@gmail.com>
2018-03-10 13:11:21 +0800

    #api #new add survey.validateDms() to parse input in different format; add test for it

--- docs/assets/js/survey.js
+++ docs/assets/js/survey.js
@@ -5,9 +5,44 @@
 var survey = {}; // group all forumla into one object to avoid duplicated namespace
 
 /**
+  * @typedef {(Object|string)} DmsLike - Expect:
+  * - An object { deg: num|str, min: num|str, sec: num|str }
+  * - Or, a string in format of 'ddd°mm\'ss\"'
+  * - Or, a tele-like string in format of 'ddd-mm-ss'
+  * - Or, an IP-like string in format of 'ddd.mm.ss'
+  */
+
+/**
+  * Validate then parse DMS (degrees-minutes-seconds) format smartly
+  * @param {DmsLike} dms
+  * @returns {Object} - In format of: { deg: num, min: num, sec: num }
+  */
+survey.validateDms = function(input) {
+	var errorStr = '[Error] Invalid input in survey.parseDms()';
+	var deg, min, sec;
+	if (typeof input === 'string') {
+		var re1 = /^\d{1,3}\xb0\d{1,2}'\d{1,2}"$/; // regex of format ddd°mm'ss"
+		var re2 = /^\d{1,3}[-.]\d{1,2}[-.]\d{1,2}$/; // regex of format ddd-dd-dd or ddd.dd.dd
+		if (re1.test(input) || re2.test(input)) {
+			var inputArr = input.split(/[\xb0'"-.]/); // regex; ['ddd', 'mm', 'ss', '*']
+			deg = +inputArr[0]; // number
+			min = +inputArr[1]; // number
+			sec = +inputArr[2]; // number
+		} else {
+			console.log(errorStr);
+			return null;
+		}
+	} else {
+			console.log(errorStr);
+			return null;
+	}
+	return { deg: deg, min: min, sec: sec };
+};
+
+/**
   * Convert degrees-minutes-seconds to decimal degrees
-  * @param {Object} dms - expect { deg: 'ddd', min: 'mm', sec: 'ss' }
-  * @returns {string} - a string consists of number from '0.0000' to '359.9999'
+  * @param {DmsLike} dms
+  * @returns {number|string} - From 0 to 359.9999
   */
 survey.dmsToDec = function(dms) {
 	return +dms.deg + (+dms.min / 60) + (+dms.sec / 3600);
--- docs/assets/js/test.js
+++ docs/assets/js/test.js
@@ -4,7 +4,46 @@
 
 console.log('Convert degrees-minutes-seconds to decimal degrees');
 console.log('Let input is 12-34-56');
-var dms = { deg: '12', min: '34', sec: '56' };
-var result = survey.dmsToDec(dms);
+var input = { deg: '12', min: '34', sec: '56' };
+var result = survey.dmsToDec(input);
 console.log('Result: ' + result);
 console.log('[done]');
+
+/**
+  * Compare value of deg, min and sec in input and expect one-by-one
+  * @function compareDms
+  * @param {Object} input - In format of: { deg: num, min: num, sec: num }
+  * @param {Object} expect - In format of: { deg: num, min: num, sec: num }
+  * @returns {boolean} - true if value of deg, min and sec are the same
+  */
+var compareDms = function(input, expect) {
+	var result = true;
+	var i, len;
+	var items = ['deg', 'min', 'sec'];
+	for (i = 0, len = items.length; i < len; i++) {
+		item = items[i];
+		result = (input[item] === expect[item] && result) ? true : false;
+	}
+	return result;
+};
+
+/**
+  * Do test of survey.validateDms(), console.log() messages and return true or false
+  * @function testValidateDms
+  * @param {string} input
+  * @param {Object} expect
+  * @returns {Object} - In format of: { deg: num, min: num, sec: num }
+  */
+var testValidateDms = function(input, expect) {
+	console.log('[test] survey.validateDms()');
+	console.log('Input: ', input);
+	var result = survey.validateDms(input);
+	var pass = compareDms(result, expect)
+	console.log('Pass: ', pass, ' Result: ', result, ' Expect: ', expect);
+	console.log('[done]');
+	return pass
+};
+
+testValidateDms('12\xb034\'56\"', { deg: 12, min: 34, sec: 56 });
+testValidateDms('12-34-56', { deg: 12, min: 34, sec: 56 });
+testValidateDms('12.34.56', { deg: 12, min: 34, sec: 56 });
